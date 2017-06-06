52f10bebedc078e9860b01966d840a54bc4d349c
johnnyniu <johnnyniu@aconex.com>
2017-06-06 18:49:34 +1000

    rotate to last active window when switch app

--- rotate.py
+++ rotate.py
@@ -25,6 +25,19 @@ all_windows_str_array =  string.split(out, '\n')
 
 to_rotate_win_ids = []
 
+LAST_ALIVE_FILE= '/var/tmp/windows-manager-last-alive-{}-id'.format(TO_ROTOATE_STR)
+
+def save_last_alive(win_id):
+    with open(LAST_ALIVE_FILE, 'w') as f:
+        f.write(win_id)
+    f.close()
+
+def get_last_alive():
+    if(os.path.isfile(LAST_ALIVE_FILE)):
+        with open(LAST_ALIVE_FILE, 'r') as f:
+            return f.read()
+    return ''
+
 def get_current_win_id():
     proc = subprocess.Popen(['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE, shell=False)
     (out,err) = proc.communicate()
@@ -32,34 +45,50 @@ def get_current_win_id():
     #different id format between wmctrl and xprop: eg: 0x03a00041 vs 0x3a00041
     return '0x' + hex_id.zfill(8)
 
-for i, window_str in enumerate(all_windows_str_array):
-    if window_str == '':
-        continue
 
-    window_data = string.split(window_str, maxsplit=4)
-    win_id = window_data[wmctrl_columns['id']]
-    win_desktop = window_data[wmctrl_columns['desktop']]
-    win_class = window_data[wmctrl_columns['wm_class']]
-    win_title = window_data[wmctrl_columns['title']]
+def setup_rotation_array_for_win_class():
+    for i, window_str in enumerate(all_windows_str_array):
+        if window_str == '':
+            continue
 
-    if TO_ROTOATE_STR in win_class:
-        to_rotate_win_ids.append(win_id)
+        window_data = string.split(window_str, maxsplit=4)
+        win_id = window_data[wmctrl_columns['id']]
+        win_desktop = window_data[wmctrl_columns['desktop']]
+        win_class = window_data[wmctrl_columns['wm_class']]
+        win_title = window_data[wmctrl_columns['title']]
+
+        if TO_ROTOATE_STR in win_class:
+            to_rotate_win_ids.append(win_id)
+
+
+setup_rotation_array_for_win_class()
 
 next_win_id = ''
 
-if len(to_rotate_win_ids)>1:
-    current_win_id = get_current_win_id()
-    print "current win id: "+ current_win_id
-    print to_rotate_win_ids
-    if(current_win_id in to_rotate_win_ids):
-        index = to_rotate_win_ids.index(current_win_id)
-        if index == len(to_rotate_win_ids) - 1:
-            next_win_id = to_rotate_win_ids[0]
+
+def get_rotate_to_win_id():
+    global current_win_id, next_win_id
+    if len(to_rotate_win_ids) > 1:
+        current_win_id = get_current_win_id()
+        is_switch_app = current_win_id in to_rotate_win_ids
+        if (is_switch_app):
+            index = to_rotate_win_ids.index(current_win_id)
+            if index == len(to_rotate_win_ids) - 1:
+                next_win_id = to_rotate_win_ids[0]
+            else:
+                next_win_id = to_rotate_win_ids[index + 1]
         else:
-            next_win_id = to_rotate_win_ids[index+1]
-    else:
-        next_win_id = to_rotate_win_ids[0]
+            logger.info("switching app------------to ---%s", TO_ROTOATE_STR)
+            last_alive_wid = get_last_alive()
+            if(last_alive_wid == ''):
+                next_win_id = to_rotate_win_ids[0]
+            else:
+                next_win_id = last_alive_wid
+
+get_rotate_to_win_id()
 
 if next_win_id !='':
-    print("going to active: ", next_win_id)
+    logger.debug("going to active: ", next_win_id)
     subprocess.call(['wmctrl', '-ia', next_win_id])
+    save_last_alive(next_win_id)
+
